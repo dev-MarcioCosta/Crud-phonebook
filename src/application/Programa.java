9c1ac4e57213f5c1a785065c2b2a5b19bb1e1b72
dev-MarcioCosta <dev.marciocosta@gmail.com>
2021-04-24 11:37:07 -0300

    Primeiro Commit

---
+++ src/application/Programa.java
@@ -0,0 +1,68 @@
+package application;
+
+import entities.Pessoa;
+
+import java.util.Scanner;
+
+public class Programa {
+
+    public static void main(String[] args)  {
+
+        Pessoa p = new Pessoa();
+
+        Scanner sc = new Scanner(System.in);
+        int opcao = 1;
+            
+                    while (opcao != 0) {
+
+            System.out.println("-------------------------------------------");
+            System.out.println("|            AGENDA TELEFÔNICA            |");
+            System.out.println("|[1]INCLUIR                   [2]ALTERAR  |");
+            System.out.println("|[3]EXCLUIR                   [4]RELATÓRIO|");
+            System.out.println("|                [0]SAIR                  |");
+            System.out.println("-------------------------------------------");
+
+            System.out.print("Escolha a opção correspondente a função:");
+            opcao = sc.nextInt();
+
+            switch (opcao) {
+             
+                case 1: 
+                    
+                    p.incluir();
+                    break;
+                    
+                case 2:
+                    
+                    p.alterar();
+                    break;
+                    
+                case 3:
+
+                    p.remover();
+                    break;
+                    
+                case 4:
+                    
+                    p.relatorio();
+                    break;
+                    
+                case 0:
+                    System.out.println("Saiu");
+                    break;
+                    
+                default:
+                    
+                    System.out.println("Opção incorreta");
+
+            }
+
+        }
+         
+        
+
+
+
+    }
+
+}
